ee4ef9c1571cad21db41e3ce8554f0a90fa888e7
johannesne <johannes.ne@gmail.com>
2018-08-03 20:23:40 +0200

    reorder for github pages

--- csv_to_json.R
+++ csv_to_json.R
@@ -41,7 +41,7 @@ litclock_list <- litclock %>%
 
 
 # save individual files
-lit_times_path <- 'times/'
+lit_times_path <- 'docs/times/'
 save_json <- function(df) {
   cat(toJSON(df, pretty = TRUE), file = paste0(lit_times_path, df$time[1], ".json"))
 }

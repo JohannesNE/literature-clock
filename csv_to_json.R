3478384d464491a824ddab7670185aed506ce2b7
johannesne <johannes.ne@gmail.com>
2018-08-12 17:51:22 +0200

    change : to _
    make csv_to_json.R windows fs compatible

--- csv_to_json.R
+++ csv_to_json.R
@@ -43,7 +43,7 @@ litclock_list <- litclock %>%
 # save individual files
 lit_times_path <- 'docs/times/'
 save_json <- function(df) {
-  cat(toJSON(df, pretty = TRUE), file = paste0(lit_times_path, df$time[1], ".json"))
+  cat(toJSON(df, pretty = TRUE), file = paste0(lit_times_path, sub(':', '_', df$time[1]), ".json"))
 }
 
 lapply(litclock_list, save_json)
